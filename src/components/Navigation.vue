de224df1dee45e4a9d368e96a39aaece9643a8ca
Yarnivz <yarnivanzeebroeck10@gmail.com>
2024-07-08 11:28:22 +0200

    configured tailwind css

---
+++ src/components/Navigation.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+    const navItems = ["Get started","Story","About us"]
+</script>
+
+<template>
+    <div>
+        <nav>
+            <div id="company-name" class="text-2xl">
+                Hestia
+            </div>
+            <div id="company-logo">
+
+            </div>
+
+            <ul>
+                <li v-for="item in navItems">
+                    {{ item }}
+                </li>
+            </ul>
+        </nav>
+    </div>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,61 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.vue"],
+  theme: {
+    colors : {
+      // THEMECOLOR ACCENT MAIN
+      'themecolor-accent-main-100' : '#99FFE9',
+      'themecolor-accent-main-200' : '#7AF0D7',
+      'themecolor-accent-main-300' : '#60E0C5',
+      'themecolor-accent-main-400' : '#47D1B4',
+      'themecolor-accent-main-500' : '#31C3A4',
+      'themecolor-accent-main-600' : '#1FA185',
+      'themecolor-accent-main-700' : '#0F7D66',
+      'themecolor-accent-main-800' : '#055947',
+      'themecolor-accent-main-900' : '#00382C',
+      // THEMECOLOR ACCENT SECONDARY
+      'themecolor-accent-secondary-100' : '#B699FF',
+      'themecolor-accent-secondary-200' : '#9B7AF0',
+      'themecolor-accent-secondary-300' : '#8460E0',
+      'themecolor-accent-secondary-400' : '#6E47D1',
+      'themecolor-accent-secondary-500' : '#5B31C3',
+      'themecolor-accent-secondary-600' : '#441FA1',
+      'themecolor-accent-secondary-700' : '#2E0F7D',
+      'themecolor-accent-secondary-800' : '#1D0559',
+      'themecolor-accent-secondary-900' : '#100038',
+      // NEUTRAL
+      'themecolor-neutral-100' : '#E5FFFA',
+      'themecolor-neutral-200' : '#D8F0EB',
+      'themecolor-neutral-300' : '#CAE0DC',
+      'themecolor-neutral-400' : '#BCD1CD',
+      'themecolor-neutral-500' : '#AEC2BE',
+      'themecolor-neutral-600' : '#90A19D',
+      'themecolor-neutral-700' : '#707D7A',
+      'themecolor-neutral-800' : '#505957',
+      'themecolor-neutral-900' : '#323837',
+      // GREEN
+      'themecolor-green-100' : '#99FFB6',
+      'themecolor-green-200' : '#7AF09B',
+      'themecolor-green-300' : '#60E084',
+      'themecolor-green-400' : '#47D16E',
+      'themecolor-green-500' : '#31C35A',
+      'themecolor-green-600' : '#1FA144',
+      'themecolor-green-700' : '#0F7D2E',
+      'themecolor-green-800' : '#05591D',
+      'themecolor-green-900' : '#003810',
+      // RED
+      'themecolor-red-100' : '#FF99AF',
+      'themecolor-red-200' : '#F07A94',
+      'themecolor-red-300' : '#E0607C',
+      'themecolor-red-400' : '#D14765',
+      'themecolor-red-500' : '#C33150',
+      'themecolor-red-600' : '#A11F3B',
+      'themecolor-red-700' : '#7D0F27',
+      'themecolor-red-800' : '#590517',
+      'themecolor-red-900' : '#38000C',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
