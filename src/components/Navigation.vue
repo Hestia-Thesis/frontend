16eeb8becff3a2aabb3fe08de8fbbe88e12b1d6a
Yarnivz <yarnivanzeebroeck10@gmail.com>
2024-07-12 13:55:13 +0200

    improved navigation style + added pagewrapper component

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,20 +1,23 @@
 <script setup lang="ts">
-    const navItems = ["Get started","Story","About us"]
+    const navItems = [
+        {name : "Get started"},
+        {name : "Story"},
+        {name : "About us"}]
 </script>
 
 <template>
-    <div>
-        <nav>
-            <div id="company-name" class="text-2xl">
+    <div class="bg-themecolor-accent-main-500 border-2 border-themecolor-accent-main-600">
+        <nav class="flex w-full justify-between items-center py-2 h-16">
+            <div id="company-name" class="text-2xl border-r-2 border-themecolor-accent-main-600  px-12 h-16 flex items-center">
                 Hestia
             </div>
             <div id="company-logo">
 
             </div>
 
-            <ul>
-                <li v-for="item in navItems">
-                    {{ item }}
+            <ul class="flex justify-between pr-5">
+                <li class="px-10" v-for="item in navItems">
+                    {{ item.name }}
                 </li>
             </ul>
         </nav>
---
+++ src/components/PageWrapper.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+</script>
+
+
+<template>
+    <div class="bg-themecolor-neutral-200 flex my-50px mx-50px md:mx-100px lg:mx-200px xl:mx-300px rounded min-h-80%">
+        <div class="min-h-[calc(100vh-100px-4rem)]">
+            
+        </div>
+    </div>
+</template>
