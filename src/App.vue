b23401c61279fb84de85159407d1b160fcecf100
Yarnivz <yarnivanzeebroeck10@gmail.com>
2024-07-19 22:34:11 +0200

    made navigation bar fixed + added Login page

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
 import Navigation from './components/Navigation.vue'
 import Index from './pages/Index.vue'
+import Login from './pages/Login.vue'
 
 </script>
 
 <template>
   <div class="bg-themecolor-neutral-100">
+    <div class="fixed w-full top-0">
+      <Navigation/>
+    </div>
     <Navigation/>
     <Index></Index>
   </div>
---
+++ src/pages/Login.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import PageWrapper from '../components/PageWrapper.vue'
+import Section from '../components/Section.vue'
+
+</script>
+
+<template>
+    <PageWrapper>
+        <Section>            
+            <div>
+                
+            </div>
+        </Section>
+    </PageWrapper>
+</template>
+
+<style>
+p {
+    margin-bottom: 18px
+}
+</style>
