e50a72f1c316a629e5f0eea6613e499ff232f257
Yarnivz <yarnivanzeebroeck10@gmail.com>
2024-08-16 23:35:00 +0200

    added VIcon component

---
+++ src/components/VIcon.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+
+const props = withDefaults(defineProps<{
+    name: string
+    filled?: boolean
+    size?: "sm" | "md" | "lg" | "xl" | "2xl" | "3xl" | "4xl" | "5xl"
+}>(), {
+    filled: false
+})
+</script>
+
+<template>
+    <span v-if="filled" class="material-icons"
+        :class="{
+            'text-sm' : size === 'sm',
+            'text-base' : size === 'md',
+            'text-lg' : size === 'lg',
+            'text-xl' : size === 'xl',
+            'text-2xl' : size === '2xl',
+            'text-3xl' : size === '3xl',
+            'text-4xl' : size === '4xl',
+            'text-5xl' : size === '5xl',
+        }"
+    >
+        {{ name }}
+    </span>
+    <span v-else class="material-symbols-outlined"
+        :class="{
+            'text-sm' : size === 'sm',
+            'text-base' : size === 'md',
+            'text-lg' : size === 'lg',
+            'text-xl' : size === 'xl',
+            'text-2xl' : size === '2xl',
+            'text-3xl' : size === '3xl',
+            'text-4xl' : size === '4xl',
+            'text-5xl' : size === '5xl',
+        }"
+    >
+        {{ name }}
+    </span>
+    
+</template>
