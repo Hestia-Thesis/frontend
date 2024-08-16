2948a164a22868a39e52fe44b352be3a9ddb68d2
Yarnivz <yarnivanzeebroeck10@gmail.com>
2024-08-16 16:54:30 +0200

    adjusted margin values

--- src/components/PageWrapper.vue
+++ src/components/PageWrapper.vue
@@ -4,8 +4,8 @@
 
 
 <template>
-    <div class="bg-themecolor-neutral-200 my-50px mx-50px md:mx-100px lg:mx-200px xl:mx-300px rounded">
-        <div class="min-h-[calc(100vh-100px-4rem)]">
+    <div class="bg-themecolor-neutral-200 sm:my-50px sm:mx-50px md:mx-100px lg:mx-200px xl:mx-300px 2xl:mx-350px rounded md:p-20px">
+        <div class="min-h-[calc(100vh-4rem)] sm:min-h-[calc(100vh-100px-4rem)] md:min-h-[calc(100vh-150px-4rem)]">
             <div class="text-black p-20px">
                 <slot>
                 </slot>                
--- src/components/Section.vue
+++ src/components/Section.vue
@@ -4,7 +4,7 @@
 
 
 <template>
-    <div class="w-[calc(full-40px)] bg-themecolor-neutral-300 my-40px mx-30px">
+    <div class="w-[calc(full-40px)] bg-themecolor-neutral-300 my-0 mx-0 sm:my-20px sm:mx-15px md:my-40px md:mx-30px">
         <div class="text-black p-20px">
             <slot>
             </slot>                
