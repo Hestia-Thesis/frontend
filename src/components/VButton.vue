5e98ffeda0ac3d620a448f9164c3ad02753d1d05
Yarnivz <yarnivanzeebroeck10@gmail.com>
2024-08-18 20:46:10 +0200

    improved style + add click event

--- src/components/VButton.vue
+++ src/components/VButton.vue
@@ -4,22 +4,36 @@
         leftIcon?: string,
         rightIcon?: string
     }>()
+
+    const emits = defineEmits<{
+        (event: "click", timeLabel: MouseEvent) : void
+    }>()
+
+    function handleClick(event : MouseEvent){
+        emits('click', event)
+    }
+
 </script>
 
 <template>
 
     <RouterLink v-if="route" :to="route">
-        <div class="inline-block bg-themecolor-accent-main-500 text-themecolor-neutral-100 hover:text-themecolor-neutral-100 
+        <div 
+        @click="handleClick"
+        class="inline-block bg-themecolor-accent-main-500 text-themecolor-neutral-100 hover:text-themecolor-neutral-100 
         border-0 border-b-4 border-r-4 active:border-2 hover:border-2 
         border-themecolor-accent-main-700 hover:border-themecolor-accent-main-700 active:bg-themecolor-accent-main-700 active:border-themecolor-accent-main-700
-        px-25px py-5px rounded-sm"
+        px-25px py-5px rounded-sm cursor-pointer select-none"
         :href="props.route">
             <slot/>
         </div>
     </RouterLink>
-    <div v-else class="inline-block bg-themecolor-accent-main-500 text-themecolor-neutral-100 hover:text-themecolor-neutral-100 
-    border-0 border-b-4 border-r-4 active:border-t-4 active:border-l-4 hover:border-2 border-themecolor-accent-main-700 hover:border-themecolor-accent-main-700 
-    px-25px py-5px rounded-sm">
+    <div v-else 
+    @click="handleClick"
+    class="inline-block bg-themecolor-accent-main-500 text-themecolor-neutral-100 hover:text-themecolor-neutral-100 
+        border-0 border-b-4 border-r-4 active:border-2 hover:border-2 
+        border-themecolor-accent-main-700 hover:border-themecolor-accent-main-700 active:bg-themecolor-accent-main-700 active:border-themecolor-accent-main-700
+        px-25px py-5px rounded-sm cursor-pointer select-none">
         <slot/>
     </div v-else>
 </template>
