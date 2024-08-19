428ae928dea3ae4c3de01b26862814aae8373562
Yarnivz <yarnivanzeebroeck10@gmail.com>
2024-08-19 22:56:51 +0200

    stores user data in UserStore on succesful login

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -4,8 +4,12 @@ import VButton from './VButton.vue';
 import { reactive, ref, computed, onMounted } from 'vue';
 import useVuelidate, { Validation } from '@vuelidate/core';
 import { required, email, helpers } from '@vuelidate/validators';
-import { User } from '../models/Models.ts';
+import { UserInterface } from '../models/Interfaces.ts';
 import { sha3_512 } from 'js-sha3';
+import { useUserStore } from '../stores/UserStore.ts';
+
+
+const userStore = useUserStore()
 
 const formData = reactive({
     email : "",
@@ -30,38 +34,36 @@ const v$ = useVuelidate(rules, formData)
 const submitForm = async () => {
     const result = await v$.value.$validate();
     if(result) {
-        alert("success")
+        alert("Succesfully logged in!")
     }
-    else {
-        alert("validation failed")
-    }
+    userStore.setUser(foundUser())
 }
 
 const stopValidation = (field : keyof typeof formData) => {
     v$.value[field].$reset();
 };
 
-const users = ref([{} as User])
+const users = ref([{} as UserInterface])
 
 const fetchUsers = () => {
     return fetch("http://127.0.0.1:8000/users")
     .then((response) => response.json())
     .catch((error) => {
-        console.log(error)
+       throw error
     })
 }
 
-const foundUser = (): User => {
+const foundUser = (): UserInterface => {
     for (const user of users.value) {
         if (user.email === formData.email) {
-        return user as User;
+        return user as UserInterface;
         }
     }
     return {
         user_id : 0,
         email : "",
         password : ""
-    } as User
+    } as UserInterface
 }
 
 function authenticate() {
@@ -83,10 +85,10 @@ onMounted(() => {
         Enter your credentials
     </div>
         <div class="my-20px">
-            <VInput v-model="formData.email" @update:modelValue="stopValidation('email')" label="Email" type="email" :validationError="!!v$.email.$errors[0]" placeholder="example@gmail.com">
+            <VInput id="email" v-model="formData.email" @update:modelValue="stopValidation('email')" label="Email" type="email" :validationError="!!v$.email.$errors[0]" placeholder="example@gmail.com">
                 <span class="text-themecolor-red-500" v-if="!!v$.email.$errors[0]">{{ v$.email.$errors[0].$message }}</span>
             </VInput>
-            <VInput v-model="formData.password" @update:modelValue="stopValidation('password')" label="Password" type="password" :validationError="!!v$.password.$errors[0]" placeholder="••••••••••••••">
+            <VInput id="password" v-model="formData.password" @update:modelValue="stopValidation('password')" label="Password" type="password" viewPassword :validationError="!!v$.password.$errors[0]" placeholder="•••••••••••••••••••••••••">
                 <span class="text-themecolor-red-500" v-if="!!v$.password.$errors[0]">{{ v$.password.$errors[0].$message }}</span>
             </VInput>
     
