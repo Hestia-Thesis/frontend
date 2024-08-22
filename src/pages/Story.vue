0942f8413c9871cfc31a6837d44c790d60b515e4
Yarnivz <yarnivanzeebroeck10@gmail.com>
2024-08-22 12:01:57 +0200

    added some functionality to story page

--- src/pages/Story.vue
+++ src/pages/Story.vue
@@ -1,6 +1,24 @@
 <script setup lang="ts">
+import { onMounted } from 'vue';
 import PageWrapper from '../components/PageWrapper.vue'
 import Section from '../components/Section.vue'
+import { useUserStore } from '../stores/UserStore';
+
+const userStore = useUserStore()
+
+const fetchEnergyData = () => {
+    return fetch("http://127.0.0.1:8000/img_story/" + userStore.user?.user_id)
+    .then((response) => response.json())
+    .catch((error) => {
+        console.log(error)
+    })
+}
+
+onMounted(() => {
+    fetchEnergyData().then((data) => {
+        //something
+    })
+})
 
 </script>
 
