dd7e82a9df6dc14f1fc90b7af7df1606f5c88d95
Yarnivz <yarnivanzeebroeck10@gmail.com>
2024-08-22 12:02:46 +0200

    made user store persist

--- src/stores/UserStore.ts
+++ src/stores/UserStore.ts
@@ -1,87 +1,86 @@
 import { defineStore } from "pinia";
+import { ref, computed, watch } from "vue";
 import { User } from "../models/Classes";
 import { UserInterface, UserDetails } from "../models/Interfaces";
 
-export const useUserStore = defineStore("user", {
-    
-    state: () => ({
-        user : {} as User | undefined,
-        hasUserDetails: false
-    }),
-    actions: {
-        async setUser(user: UserInterface) {
-            const userObj = {
-                user_id: user.user_id,
-                email: user.email,
-                password: user.password,
-                userDetails: {
-                    user_id: user.user_id,
-                    bedrooms: 0,
-                    house_value: 0,
-                    no_of_children: 0,
-                    tot_ppl: 0,
-                    employment_full_time_employee: 0,
-                    employment_part_time_employee: 0,
-                    employment_retired: 0,
-                    employment_self_employed: 0,
-                    employment_student: 0,
-                    employment_unemployeed_seeking_work: 0,
-                    family_structure_1_non_pensioner: 0,
-                    family_structure_all_pensioners: 0,
-                    family_structure_all_students: 0,
-                    family_structure_couple_with_dependent_children: 0,
-                    family_structure_other: 0,
-                    family_structure_single_parent_dependent_children: 0,
-                    savings_just_managing: 0,
-                    savings_saving_a_lot: 0,
-                    savings_saving_little: 0,
-                    savings_using_savings_in_debt: 0,
-                    house_type_bungalow: 0,
-                    house_type_detached_house: 0,
-                    house_type_flat_maisonette: 0,
-                    house_type_semi_detached: 0,
-                    house_type_terraced: 0,
-                } as UserDetails
-            } as User
-            this.user = userObj
-            this.setUserDetails()
-        },
-        clearUser() {
-            this.user = undefined
-        },
-        setUserDetails() {
-            if(this.user != undefined) {
-                    fetch("http://127.0.0.1:8000/user_details" + this.user.user_id)
-                    .then((response) => response.json())
-                    .then((data) => {
-                        this.user!.userDetails = data[0];
-                        this.hasUserDetails = true;
-                    })
-                    .catch((error) => {
-                        throw new Error(error)
-                    })
-            }
-            else {
-                throw new Error("No user is logged in")
-            }
-            
+export const useUserStore = defineStore("user", () => {
+    // State
+    const user = ref<User | undefined>(undefined);
+    const hasUserDetails = ref(false);
+
+    // Actions
+    const setUser = async (userInput: UserInterface) => {
+        const userObj: User = {
+            user_id: userInput.user_id,
+            email: userInput.email,
+            password: userInput.password,
+            userDetails: {
+                user_id: userInput.user_id,
+            } as UserDetails,
+        };
+        user.value = userObj;
+        setUserDetails();
+    };
+
+    const clearUser = () => {
+        user.value = undefined;
+        hasUserDetails.value = false;
+    };
+
+    const setUserDetails = () => {
+        if (user.value != undefined) {
+            fetch("http://127.0.0.1:8000/user_details" + user.value.user_id)
+                .then((response) => response.json())
+                .then((data) => {
+                    user.value!.userDetails = data[0];
+                    hasUserDetails.value = true;
+                })
+                .catch((error) => {
+                    throw new Error(error);
+                });
+        } else {
+            throw new Error("No user is logged in");
         }
-        
-    },
-    getters : {
-        getUserWithoutDetails: (state) => {
-            if(state.user != undefined)
+    };
+
+    // Getters
+    const getUserWithoutDetails = computed<UserInterface | undefined>(() => {
+        if (user.value != undefined) {
             return {
-                user_id: state.user.user_id,
-                email: state.user.email,
-                password: state.user.password
-            } as UserInterface
-        },
-        getUser: (state) => {
-            return state.user
-        },
-        isAuthenticated: (state) => {
-            return !!state.user?.user_id
+                user_id: user.value.user_id,
+                email: user.value.email,
+                password: user.value.password,
+            };
         }
+    });
+
+    const getUser = computed(() => user.value);
+
+    const isAuthenticated = computed(() => !!user.value?.user_id);
+    
+    const userFromStorage = localStorage.getItem("user");
+
+    if (userFromStorage) {
+        user.value = JSON.parse(userFromStorage);
     }
-})
+
+    watch(
+        user,
+        (userVal) => {
+            localStorage.setItem("user", JSON.stringify(userVal));
+        },
+        { deep: true }
+    )
+
+    // Return the state, actions, and getters
+    return {
+        user,
+        hasUserDetails,
+        setUser,
+        clearUser,
+        setUserDetails,
+        getUserWithoutDetails,
+        getUser,
+        isAuthenticated,
+    };
+});
