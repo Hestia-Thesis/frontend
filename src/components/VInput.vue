48bb8c51a5211cc476fd7e8d123f4ad96556c288
Yarnivz <yarnivanzeebroeck10@gmail.com>
2024-08-25 21:01:56 +0200

    Changed margin for metrics when size is bigger than sm

--- src/components/VInput.vue
+++ src/components/VInput.vue
@@ -82,7 +82,7 @@ import VIcon from './VIcon.vue';
                     'pl-25px' : icon,
                     'pr-25px' : props.type == 'password' && props.viewPassword,
                     'pr-30px' : props.metric && size == 'xs' || 'sm',
-                    'pr-50px' : props.metric && size == 'md' || 'lg',
+                    'pr-350px' : props.metric && size == 'md' || 'lg',
                     'border-themecolor-neutral-500' : !props.validationError && !props.disabled,
                     '!border-themecolor-red-500 !bg-themecolor-red-100 placeholder:text-themecolor-red-500' : props.validationError,
                     'bg-themecolor-neutral-100' : !props.disabled,
