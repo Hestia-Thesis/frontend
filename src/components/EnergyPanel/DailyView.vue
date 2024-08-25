9dae55fdb32c6a55dbfc3cce0094d13f332f5bbe
Yarnivz <yarnivanzeebroeck10@gmail.com>
2024-08-25 23:45:04 +0200

    fixed loader

--- src/components/EnergyPanel/DailyView.vue
+++ src/components/EnergyPanel/DailyView.vue
@@ -234,8 +234,9 @@ onMounted(() => {
             <VButton v-if="!isProcessing" @click="submitForm" type="submit" class="mb-20px text-base font-semibold">
                 Save data
             </VButton>
-            <VButton v-else disabled type="submit" class="mb-20px text-base font-semibold animate-spin">
+            <VButton v-else disabled type="submit" class="mb-20px text-base font-semibold">
                 <svg class="animate-spin h-5 w-5 mr-3 ..." viewBox="0 0 24 24">
+                    <!-- ... -->
                 </svg>
                 Processing...
             </VButton>
